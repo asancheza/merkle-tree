fa5b80bb59f33cf6e64eaf45dc22daf4ce0d4c8c
Alejandro Sanchez Acosta <asanchez@neurowork.net>
2016-03-19 14:10:54 -0700

    Update documentation in README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# merkle-tree
+# Merkle Tree
+
+Merkle implementation. 
+
+## Example
+
+Basic example: abcd => h(h(a,b) h(c,d))
+
+## Run
+
+```
+python merkle.py
+```
--- merkle.py
+++ merkle.py
@@ -2,18 +2,22 @@
 
 import hashlib
 
+def md5(chunk):
+	hash_md5 = hashlib.md5()
+	hash_md5.update(chunk)
+
+	return hash_md5.hexdigest()
+
 tree = [ 'a', 'b', 'c', 'd', 'e', 'f' ]
 
-def md5(chunk):
-    hash_md5 = hashlib.md5()
-    hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+if tree % 2 == 1:
+  tree.append(tree[-1])
 
 while (len(tree) > 1):
 	new_tree = []
 
 	for i in xrange(len(tree) / 2):
-	  new_tree.append(md5(tree[i*2]) + md5(tree[i*2 + 1]))
+	  new_tree.append(md5(tree[i * 2]) + md5(tree[i * 2 + 1]))
 
 	tree = new_tree
 
